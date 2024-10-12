9ede9c43f93fe485a94e8093980c994120cad6ec
amantiwari9413 <tiwariaman9413@gamil.com>
2024-10-12 12:16:16 +0530

    utils

--- src/db/dbCon.js
+++ src/db/dbCon.js
@@ -1,15 +1,10 @@
 import mongoose from "mongoose";
-import app from "../app.js";
 import DB_NAME from '../constant.js'
-
 
 export default async function dbConnection(){
     try {
-        const res=await mongoose.connect(`${process.env.DB_URL}/${DB_NAME}`)
+        const res=await mongoose.connect(`${process.env.DB_URL}`)
         console.log(`db is running on port${res.connection.port}`)
-        app.listen(process.env.PORT,()=>{
-            console.log("server is running on port 3000");
-        })
     } catch (error) {
         console.log(error)
         process.exit(1);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import 'dotenv/config'
 import app from './app.js';
 import dbConnection from "./db/dbCon.js"
-dbConnection();
+dbConnection()
+.then((res)=>{
+    app.listen(process.env.PORT,()=>{
+        console.log("server is running on port 3000");
+    })
+})
+.catch((error)=>{
+    console.log(error)
+})
 
 app.use('/',(req,res,error)=>{
     res.json({name:"aman",
         age:20
     })
 })
+
+
+
